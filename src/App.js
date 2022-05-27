8e5f67040a7673a08f585631748be9d371eac3fe
mehrotrasuyash <suyashmehrotra1510@gmail.com>
2022-05-27 13:42:25 +0530

    useEffect added - to maintain the session of the logged in user

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      //==> 2. To ensure that user remains logged in.
+      //** Reason it is written inside useEffect hook, is because if written outside in the main function, it will cause re-rendering thus leading to infinite loop.
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  //The below function gets triggered on login button click
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1"); //Could've gone for cookies as well! ==> 1. to make sure user remains logged in when page reloads
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn"); //==> 3. to ensure that local storage gets cleared once we click the logout button.
     setIsLoggedIn(false);
   };
 
